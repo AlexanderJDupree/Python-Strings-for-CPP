5f178dbb057d38602d8a3b3e57f956e0c966a475
Alexander DuPree <alexander.j.dupree@gmail.com>
2018-06-15 19:31:22 -0700

    Implemented Catch2 framework, added .vscode files

---
+++ tests/string_tests.cpp
@@ -0,0 +1,21 @@
+/*
+File: string_tests.cpp
+
+Copyright (c) 2018 Alexander DuPree
+
+This software is released as open source through the MIT License
+
+Authors: Alexander DuPree
+
+https://github.com/AlexanderJDupree/Python-Strings-for-CPP
+
+*/
+
+#include <iostream>
+#include "catch.hpp"
+
+TEST_CASE("HELLO WORLD!")
+{
+    std::cout << "Hello World!" << std::endl;
+
+}
